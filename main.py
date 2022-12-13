a1b709aae4ebd29ee06e6baf3a778f77ee93ae52
Kosachev Daniil <kkosachevds@gmail.com>
2022-12-13 17:39:36 +0300

    collect events

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import uuid
+import codecs
 from impacket.dcerpc.v5 import epm, even6, transport, dtypes, rpcrt, ndr
 
 from binxml import ResultSet
 import mseven6ext
+from datetime import datetime
 
 _IFACE_UUID = even6.MSRPC_UUID_EVEN6
 _EVT_SEEK_RELATIVE_TO_FIRST = 0x00000001
 
 _HOST = "172.30.254.52"
-_USERNAME = "user"
-_PASSWORD = "ms-even6"
-_DOMAIN = ""
-_CHANNEL = "Application"
+_USERNAME = "reader"
+_PASSWORD = "P@ssw0rd"
+_DOMAIN = "."
+_CHANNEL = "Security"
+_BATCH_SIZE = 31
+_EPS_WRITE_INTERVAL_SEC = 2
 
 
 def main():
@@ -23,28 +26,44 @@ def main():
     dce.connect()
     dce.bind(_IFACE_UUID)
 
-    event = mseven6ext.EventDescriptor()
-    event['Id'] = 400
-    event['Version'] = 0
-    event['Channel'] = 0
-    event['Level'] = 4
-    event['Opcode'] = 0
-    event['Task'] = 0
-    event['Keyword'] = 0x8000000000000000
+    request = even6.EvtRpcRegisterLogQuery()
+    request['Path'] = _CHANNEL + '\x00'
+    request['Query'] = "*" + '\x00'
+    request['Flags'] = even6.EvtQueryChannelName | even6.EvtReadNewestToOldest
 
-    event_id = ndr.NDRUniConformantArray()
-    event_id['Data'] = event.getData()
+    resp = dce.request(request)
+    log_handle = resp['Handle']
+    events = []
 
-    req = mseven6ext.EvtRpcMessageRenderDefault()
-    req['SizeEventId'] = len(event_id)
-    req['EventId'] = event_id
-    req['MessageId'] = 0xffffffff  # -1
-    req['Values'] = mseven6ext.EvtRpcVariantList()
-    req['Flags'] = 0x00000002
-    req['MaxSizeString'] = 1024
+    last_eps_write_time = datetime.now()
+    eps_count = 0
+    while True:
+        request = even6.EvtRpcQueryNext()
+        request['LogQuery'] = log_handle
+        request['NumRequestedRecords'] = _BATCH_SIZE
+        request['TimeOutEnd'] = 1000
+        request['Flags'] = 0
+        resp = dce.request(request)
+        num_records = resp['NumActualRecords']
+        if num_records == 0:
+            break
+        for i in range(num_records):
+            event_offset = resp['EventDataIndices'][i]['Data']
+            event_size = resp['EventDataSizes'][i]['Data']
+            event = resp['ResultBuffer'][event_offset:event_offset + event_size]
+            event_bytes = b''.join(event)
+            events.append(ResultSet(event_bytes).xml())
 
-    resp = dce.request(req)
-    print(b''.join(resp['String']).decode('utf-16'))
+            eps_count += 1
+            now = datetime.now()
+            diff_sec = (now - last_eps_write_time).total_seconds()
+            if diff_sec >= _EPS_WRITE_INTERVAL_SEC:
+                print("EPS: ", eps_count / diff_sec)
+                eps_count = 0
+                last_eps_write_time = now
+
+    with codecs.open("data/events.txt", "w", encoding="utf-8") as f:
+        f.write("\n".join(events))
 
 
 if __name__ == "__main__":
